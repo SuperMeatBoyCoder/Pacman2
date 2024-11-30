a54678aa576e32c67f5debdc803aab49ea8a6ea8
SuperMeatBoyCoder <marat-ashirov@list.ru>
2024-11-30 12:44:17 +0300

    added finger tracking

--- finger_detector.py
+++ finger_detector.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -5,28 +7,37 @@ import numpy as np
 #создаем детектор
 handsDetector = mp.solutions.hands.Hands()
 cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
+i = 0
+while True:
+    i += 1
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
-    flipped = np.fliplr(frame)
+    imgBGR = np.fliplr(frame)
     # переводим его в формат RGB для распознавания
-    flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+    m, n = imgRGB.shape[:2]
     # Распознаем
-    results = handsDetector.process(flippedRGB)
-    # Рисуем распознанное, если распозналось
+    results = handsDetector.process(imgRGB)
+    index_tip = (0, 0)
+    thumb_tip = (n, m)
     if results.multi_hand_landmarks is not None:
-        # нас интересует только подушечка указательного пальца (индекс 8)
-        # нужно умножить координаты а размеры картинки
-        x_tip = int(results.multi_hand_landmarks[0].landmark[8].x *
-                flippedRGB.shape[1])
-        y_tip = int(results.multi_hand_landmarks[0].landmark[8].y *
-                flippedRGB.shape[0])
-        cv2.circle(flippedRGB,(x_tip, y_tip), 10, (255, 0, 0), -1)
-        print(results.multi_hand_landmarks[0])
+        index_tip = results.multi_hand_landmarks[0].landmark[8]
+        index_tip = int(index_tip.x * n), int(index_tip.y * m)
+        thumb_tip = results.multi_hand_landmarks[0].landmark[4]
+        thumb_tip = int(thumb_tip.x * n), int(thumb_tip.y * m)
+    imgRGB.fill(0)
+    cv2.circle(imgRGB, index_tip, 5, (255, 0, 0), -1)
+    cv2.circle(imgRGB, thumb_tip, 5, (255, 0, 0), -1)
+    dis = (thumb_tip[0] - index_tip[0]) ** 2 + (thumb_tip[1] - index_tip[1]) ** 2
+    tap_point = (-1, -1)
+    if dis < 2000:
+        tap_point = (int((thumb_tip[0] + index_tip[0]) / 2), int((thumb_tip[1] + index_tip[1]) / 2))
+        cv2.circle(imgRGB, tap_point, 5, (0, 255, 0), -1)
+
     # переводим в BGR и показываем результат
-    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-    cv2.imshow("Hands", res_image)
+    imgBGR = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2BGR)
+    cv2.imshow("Hands", imgBGR)
 
 # освобождаем ресурсы
 handsDetector.close()
